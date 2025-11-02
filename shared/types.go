e675f851847799ca01f4c6067fe91bd2655fd1cf
Erick <erickcarpesmmc@gmail.com>
2025-11-02 15:45:15 -0300

    Refactor RPC contracts in types.go for improved clarity and consistency; enhance comments for better understanding

--- shared/types.go
+++ shared/types.go
@@ -1,37 +1,44 @@
-// shared/types.go
 package shared
 
-// PlayerState é a única coisa que o servidor entende
+// Estado do jogador
 type PlayerState struct {
 	PosX int
 	PosY int
 }
 
-// === RPC: Connect ===
+// Contrato que o cliente manda para se conectar com o servidor
 type ConnectArgs struct{}
+
+// Resposta do servidor ao conectar um novo jogador
 type ConnectReply struct {
 	PlayerID   int
 	AllPlayers map[int]PlayerState // Todos os jogadores, incluindo o novo
 }
 
-// === RPC: UpdateState (Cliente-Autoritativo) ===
+// Contrato para atualizar o estado do jogador
 type UpdateStateArgs struct {
 	PlayerID       int
 	NewX           int
 	NewY           int
 	SequenceNumber int
 }
+
+// Resposta do servidor à atualização de estado
 type UpdateStateReply struct{}
 
-// === RPC: GetState ===
+// Contrato para obter o estado de todos os jogadores
 type GetStateArgs struct{}
+
+// Resposta do servidor com o estado de todos os jogadores
 type GetStateReply struct {
 	AllPlayers map[int]PlayerState
 }
 
-// === RPC: Disconnect ===
+// Contrato para desconectar um jogador
 type DisconnectArgs struct {
 	PlayerID       int
 	SequenceNumber int
 }
+
+// Resposta do servidor à desconexão
 type DisconnectReply struct{}
